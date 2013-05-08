7d7d42152a71bb6058a9bcdc23dfd9f6b28914fd
Youri van den Bogert <youri@vandenbogert.eu>
2013-05-08 09:44:03 +0200

    CustomJS variable was declared, but never used. Added functionality. Now possible to give multiply custom variables through a array. Updated readme.

--- Piwik.php
+++ Piwik.php
@@ -17,7 +17,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 $wgExtensionCredits['other'][] = array(
 	'path'           => __FILE__,
 	'name'           => 'Piwik Integration',
-	'version'        => '2.2.0',
+	'version'        => '2.2.1',
 	'author'         => array('Isb1009', '[http://www.daschmedia.de DaSch]', '[https://github.com/YOUR1 Youri van den Bogert]'),
 	'description'	 => 'Adding Piwik Tracking Code',
 	'url'            => 'https://www.mediawiki.org/wiki/Extension:Piwik_Integration',
