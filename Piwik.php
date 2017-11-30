3199a39e6ada4e46e40f2509a1fd31868121e75c
Seb35 <seb35@seb35.fr>
2017-11-30 16:32:41 +0100

    Version 2.4.4
    The branch 2.x will contain versions compatible with MediaWiki 1.24 and older;
    the branch 3.x will be compatible with MediaWiki 1.25+.

--- Piwik.php
+++ Piwik.php
@@ -17,7 +17,7 @@ if ( !defined( 'MEDIAWIKI' ) ) die( 'This file is a MediaWiki extension, it is n
 $GLOBALS['wgExtensionCredits']['other'][] = array(
 	'path'           => __FILE__,
 	'name'           => 'Piwik Integration',
-	'version'        => '2.4.2',
+	'version'        => '2.4.4',
 	'author'         => array('Isb1009', '[http://www.daschmedia.de DaSch]', '[https://github.com/YOUR1 Youri van den Bogert]'),
 	'description'    => 'Adding Piwik Tracking Code',
 	'url'            => 'https://www.mediawiki.org/wiki/Extension:Piwik_Integration',
